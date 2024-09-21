9738d709e49e825101e864b32a24e3249c28a2d8
Abdykali Azat <azat.abdykalli@gmail.com>
2024-09-22 01:20:22 +0500

    first commit

---
+++ src/components/dashboard/MyCargo.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import './css/admin.css';
+import Title from "./title";
+import StatusList from "./StatusList";
+import FilialList from "./FilialList";
+import Settings from "./Settings";
+import Weight from "./Weight"
+
+const MyCargo = () => {
+
+    return (
+      
+        <div className="my-cargo">
+            <Title text="Мой карго"/>
+            <div className="section-my-cargo">
+                <Settings/>
+                <Weight/>
+            </div>
+            <div className="section-my-cargo">
+                <StatusList/>
+                <FilialList/>
+
+            </div>
+
+
+        </div>
+
+    )
+}
+
+export default MyCargo;
